eb3add14253f27c678e45ca60626f023f597915e
louis01056 <louis01056@gmail.com>
2022-11-14 12:40:22 -0800

    distanceupdate

--- include/Circle.h
+++ include/Circle.h
@@ -28,7 +28,7 @@ class Circle : public Containable {
    double radius;
 };
 
-double distance(const Point& p1, const Point& p2){
+static double distance(const Point& p1, const Point& p2){
     return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
 
