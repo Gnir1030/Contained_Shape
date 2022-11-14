c3579cde3291449393dcb37f066753263b37d08c
louis01056 <louis01056@gmail.com>
2022-11-14 12:41:55 -0800

    distanceupdate

--- include/Circle.h
+++ include/Circle.h
@@ -28,8 +28,4 @@ class Circle : public Containable {
    double radius;
 };
 
-static double distance(const Point& p1, const Point& p2){
-    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
-}
-
 #endif
--- src/Circle.cc
+++ src/Circle.cc
@@ -3,11 +3,9 @@
 #include "Reuleaux.h"
 //#include <cmath>
 
-/*
 double distance(const Point& p1, const Point& p2){
     return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
-*/
 
     // do not modify or remove this constructor
 Circle::Circle(const Point &center, double radius){
