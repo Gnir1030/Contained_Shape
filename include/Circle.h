faed058df080093b039e2e0059c96dbd47a7a159
louis01056 <louis01056@gmail.com>
2022-11-14 12:31:51 -0800

    makeupdate

--- include/Circle.h
+++ include/Circle.h
@@ -32,8 +32,4 @@ double distance(const Point& p1, const Point& p2){
     return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
 
-double absolute(double value){
-  return value < 0 ? -value : value;
-}
-
 #endif
