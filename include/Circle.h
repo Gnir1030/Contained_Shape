18f0731d0085600624c8c063f65a7ec4a56087c1
louis01056 <louis01056@gmail.com>
2022-11-14 14:04:36 -0800

    frienddistance

--- include/Circle.h
+++ include/Circle.h
@@ -10,20 +10,21 @@
 
 #include "Containable.h"
 #include "Point.h"
-#include "distance.h"
 #include <math.h>
 
 class Circle : public Containable {
   public:
     // do not modify or remove this constructor
     Circle(const Point &center, double radius);
-
     // do not modify or remove these functions
     bool ContainedBy(Circle &circle);
     bool ContainedBy(Polygon &polygon);
     bool ContainedBy(ReuleauxTriangle &rt);
     Point getCen();
     double getRad();
+    friend double distance(const Point& p1, const Point& p2){
+        return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+    }
   private:
    Point center;
    double radius;
