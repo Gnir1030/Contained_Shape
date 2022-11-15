5064ad6ba0c0792b07a53b223042112b03f3a1e7
louis01056 <louis01056@gmail.com>
2022-11-14 16:53:26 -0800

    PolygonCircle

---
+++ tests/PolygonCircle.cc
@@ -0,0 +1,39 @@
+#include "Circle.h"
+#include "Polygon.h"
+#include <gtest/gtest.h>
+// Uncomment when you're ready
+
+std::vector<Point> convex = {Point(0.0,0.0), Point(2.0, 0.0), Point(3.0, 2.0), Point(1.0, 4.0), Point(-1.0, 2.0)};
+
+TEST(PolygonCircle, Contained)
+{
+  Polygon inner = Polygon(convex);
+  Circle outer = Circle(Point(1.0,2.0), 4.0);
+  ASSERT_TRUE(inner.ContainedBy(outer));
+}
+
+TEST(PolygonCircle2, Case1NotContained)
+{
+  Polygon inner = Polygon(convex);
+  Circle outer = Circle(Point(1.0,2.0), 1.0);
+  ASSERT_FALSE(inner2.ContainedBy(outer));
+}
+
+TEST(PolygonCircle3, Case2NotContained)
+{
+  Polygon inner = Polygon(convex);
+  Circle outer = Circle(Point(2.0,2.0), 1.5);
+  ASSERT_FALSE(inner2.ContainedBy(outer));
+}
+
+TEST(PolygonCircle4, Case3NotContained)
+{
+  Polygon inner = Polygon(convex);
+  Circle outer = Circle(Point(1.0,2.0), sqrt(3.0));
+  ASSERT_FALSE(inner2.ContainedBy(outer));
+}
+/* 
+    1. All Polygon vertices outside Circle (Inner surrounds Outer)
+    2. One or more Polygon vertices outside Circle (Inner and Outer intersect) 
+    4. One or more Polygon vertices touch Outer perimeter
+*/
