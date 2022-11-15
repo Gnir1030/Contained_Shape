376228fa66904d425a0eae2ecbe1772034ea67e0
louis01056 <louis01056@gmail.com>
2022-11-14 22:36:19 -0800

    Reuleaux

--- src/Reuleaux.cc
+++ src/Reuleaux.cc
@@ -36,18 +36,25 @@ bool ReuleauxTriangle::ContainedBy(Polygon &polygon){
     for(auto k : vertices){
         auto j = *(polygon.getVer().end() - 1);
         bool oddEdges= false;
-        //double Dist;
+        double maxDist = 0;
         for (auto i : polygon.getVer()) {
             if ((i.y < k.y && j.y >= k.y) ||  (j.y < k.y && i.y >= k.y )) {
                 if (i.x + (k.y-i.y)/(j.y - i.y)*(j.x - i.x) < k.x) {
                     oddEdges=!oddEdges; 
                 }
             }
-            //Dist = std::abs((j.x - i.x) * k.y + (i.y - j.y) * k.x + (i.x * j.y - j.x * i.y))/sqrt(pow(i.y - j.y, 2) + pow(j.x - i.x, 2));
+            double Dist = std::abs((j.x - i.x) * k.y + (i.y - j.y) * k.x + (i.x * j.y - j.x * i.y))/sqrt(pow(i.y - j.y, 2) + pow(j.x - i.x, 2));
+            if(maxDist < Dist){
+                maxDist = Dist;
+            }
             j=i;
         }
     
         if(!oddEdges){
+            return false;
+        }
+
+        if(maxDist <= radius){
             return false;
         }
     }
